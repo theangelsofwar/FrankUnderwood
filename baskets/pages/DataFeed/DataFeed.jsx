04e751f87a91e92004bbd35711905eb4462fd4e2
theangelsofwar <angiechangpagne@gmail.com>
2020-06-01 19:01:02 -0700

    styled newsfeed

--- baskets/pages/DataFeed/DataFeed.jsx
+++ baskets/pages/DataFeed/DataFeed.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Dimensions, RefreshControl } from 'react-native';
-import api from '../../services/api';
+import api from '../../../services/api';
 
 
 import {
---
+++ baskets/pages/DataFeed/styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const TouchableOpacity = styled.TouchableOpacity`
+  margin: 20px;
+`
+
+export const Image = styled.Image``;
+
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const FlatList = styled.FlatList``;
+
+
+export const FeedItem = styled.View`
+  margin: 10px 0;
+`;
+
+export const Header = styled.View`
+
+`;
+export const Name = styled.Text`
+`;
